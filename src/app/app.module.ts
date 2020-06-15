14d8f3179caf1de0b86ea13db3084ad164bc98b0
Marko Miseljic <marko.miseljic.14@gmail.com>
2020-06-15 09:26:12 +0200

    Frontend files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './service/product.service';
+
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes =[
+  {path:'search/:keyword',component: ProductListComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'products/:id',component: ProductDetailsComponent},
+  {path:'category/:id',component: ProductListComponent},
+  {path:'category',component: ProductListComponent},
+  {path:'products',component: ProductListComponent},
+  {path:'cart-details',component: CartDetailsComponent},
+  {path:'',redirectTo: '/products',pathMatch: 'full'},
+  {path:'**',redirectTo:'/products',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    CartStatusComponent,
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
